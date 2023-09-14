274b229fcb9013b598ea46781bc4a50cc2f80fcd
joaquin-alvarez21 <joaquin.alvarez@uc.cl>
2023-09-14 16:49:00 -0300

    add home page template of auth0

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Container } from "reactstrap";
+
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from './viws/Home';
+import Profile from './viws/Profile';
+
+//styles
+import './App.css';
+
+// fontawesome
+import initFontAwesome from "./utils/initFontAwesome";
+initFontAwesome();
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div id="app" className="d-flex flex-column h-100">
+        <NavBar />
+        <Container className="flex-grow-1 mt-5">
+          <Routes>
+            <Route path="/"  element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
